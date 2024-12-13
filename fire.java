8ba53b313bb44846263103883230469cc41d2822
Tharak Ram <tharakarame@gmail.com>
2024-12-13 10:25:14 +0530

    Add files via upload

---
+++ fire.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class fire
+{
+    public static void main(String args[])
+    {
+        LinkedList<String>a=new  LinkedList<>();
+    a.add("hello");
+    a.add("i am");
+    a.add("poorna");
+    System.out.println(a);    
+    
+    a.remove(0);
+    System.out.println(a);
+    }
+}
